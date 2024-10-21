1ee466ed5c0b4cb90575c3ac85a6281e0f37e1af
felexfox <koreckiimiroslav60@gmail.com>
2024-10-21 15:12:39 +0500

    Всё с самого начала.

---
+++ src/main/java/me/flx/xzxc/XZXC.java
@@ -0,0 +1,23 @@
+package me.flx.xzxc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public final class XZXC extends JavaPlugin {
+
+    private static XZXC instance;
+
+    @Override
+    public void onEnable() {
+    }
+
+    public static XZXC getPlugin() {
+        return instance;
+    }
+
+    @Override
+    public void onDisable(){
+        // эта хуйня когда-то пригодится для рецептов, хд
+    }
+}
